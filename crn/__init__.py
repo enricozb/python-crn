5082644a83191747a6a9e2959305c62648070021
Enrico Borba <enricozb@gmail.com>
2018-03-10 20:56:19 -0800

    Added aTAM support
    Basic tile support, syntax is ok. Would also like to eventually support
    matplotlib plotting of tiles with animation of assembly.

--- crn/__init__.py
+++ crn/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ["species", "CRN", "schemas"]
+__all__ = ["species", "CRN", "schemas", "Tile", "TileSystem"]
 
 from crn.reaction import *
 from crn.simulation import *
 import crn.utils as utils
 utils.stochpy_fix()
 from crn.crn import *
+from crn.tile import *
 
